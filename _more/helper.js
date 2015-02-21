fbc6780dae329ee11be1b37c7ed845ad9b1ee996
Max Fechner <max.fechner@gmail.com>
2015-02-21 21:42:36 +0700

    clean up 2...

--- _more/helper.js
+++
@@ -1,6 +0,0 @@
-        function setVal(obj, attr, key, val){
-            var dot = document.querySelector(obj);
-            var tmp = dot[attr];
-            tmp[key] = val;
-            dot.setAttribute(attr,JSON.stringify(tmp) );
-        }
